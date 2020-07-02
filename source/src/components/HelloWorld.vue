6e20d19da00e58e9a65a4176b70dbd9a5d3cc5ee
Thao Nguyen <vanthao10a8@gmail.com>
2020-07-02 09:03:54 +0700

    init source

---
+++ source/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <select v-model="currentLocale">
+      <option v-for="locale in locales" v-bind:value="locale.id" v-bind:key="locale.id">{{locale.name}}</option>
+    </select>
+    <p>{{$t('Hello')}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+      currentLocale : 'en',
+      locales: [ {id: 'en', name: 'English'}, {id: 'vi', name: 'Viet Nam'}]
+    }
+  },
+  watch : {
+    currentLocale: function(){
+      this.$i18n.locale = this.currentLocale;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
